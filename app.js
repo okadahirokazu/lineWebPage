9dcad2795fb17df02deeb6b49831c6060ebc7c72
HirokazuOkada <okada.hirokazu@ditgroup.jp>
2017-11-28 10:03:36 +0900

    modify app.js

--- app.js
+++ app.js
@@ -3,16 +3,9 @@ var app = angular.module('myApp', ['onsen.directives']);
 
 // アプリ初期処理
 app.init = function(){
-	// Nodeモジュール
-	// 必要な場合セット
-	// 独自モジュール
-	app.util = util;
-	// アプリ内項目データをセット
-	app.data = {};
-	// アプリ内共通オブジェクト
-	app.views = {};
-	// AngularJSアプリ開始
-	app.start();
+	app.data = {};	// アプリ内項目データをセット
+	app.views = {};	// アプリ内共通オブジェクト
+	app.start();	// AngularJSアプリ開始
 };
 
 // AngularJSアプリとして起動
